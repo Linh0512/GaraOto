1c693259e13ea93c434a136902606bd65ba15075
Linh0512 <quilinh831@gmail.com>
2024-12-02 22:08:10 +0700

    .

---
+++ WinFormsApp/DAO/ServiceDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp.DAO
+{
+    internal class ServiceDAO
+    {
+        public static ServiceDAO instance;
+
+        public DataTable FindBaseName(string baseName)
+        {
+            string query = "SELECT * FROM dbo.XE WHERE HieuXe = N'" + baseName + "'";
+            return DataProvider.instance.ExecuteQuery(query);
+        }
+    }
+}
