7a25128cb129f9709cfe7517b834afdbbe478a8b
Võ Chính Hiệu <vochinhhieu11082004@gmail.com>
2024-12-08 22:53:45 +0700

    upload phần model và phần tính năng báo cáo

---
+++ WinFormsApp/Models/PhuTung.cs
@@ -0,0 +1,10 @@
+﻿namespace WinFormsApp.Models
+{
+    public class PhuTung
+    {
+        public string MaVTPT { get; set; }
+        public string TenVTPT { get; set; }
+        public int SoLuongTon { get; set; }
+        public decimal DonGia { get; set; }
+    }
+}
